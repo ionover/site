e5ffce61a85186e317d28c036166939d84dbeece
v.ionov <v.ionov@mycrg.ru>
2026-03-11 18:05:35 +0300

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_ACTIONS === "true";
+const repoName = "site";
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubPages ? `/${repoName}` : "",
+  assetPrefix: isGithubPages ? `/${repoName}/` : undefined,
 };
 
 export default nextConfig;
